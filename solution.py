f91f70f93984fd2a96d0054e2c2f34efa01c98ff
Epishina Anastasya <62638634+an8st@users.noreply.github.com>
2024-05-10 16:56:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,9 +5,4 @@ from scipy.stats import ks_2samp
 chat_id = 805920792 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:    
-    stat, p = ks_2samp(x, y)
-    if p <= 0.08:
-        b = True
-    else:
-        b = False
-    return b
+    return anderson_ksamp([x, y]).pvalue < 0.08
